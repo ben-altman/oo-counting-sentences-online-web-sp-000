980ffa05af4c06e5b4c03743cb7669400819b5eb
Ben Altman <altmanguitar@gmail.com>
2020-12-13 01:06:23 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,23 @@ require 'pry'
 
 class String
 
+# return true if a string ends with a sentence, false if it doesn't
+# use #self keyword in body to refer to string on which the method is being called
+# use #end_with? to determine final character
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(". ", "? ", "! ")
+    array.count
   end
 end
